d275ab4e812858bac432c4650e361dbefe8da6c1
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-30 16:13:36 -0500

    working

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,25 +1,55 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Student {
 
-    public long id;
-    public String user;
+    public static String name;
+    public static long id;
+    public static ArrayList<Integer> grades;
 
-    public Student(long newId, String newUser){
-        this.id = newId;
-        this.user = newUser;
+//    private long id;
+//    private String name;
+//    private ArrayList<Integer> grades;
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double bucket = 0;
+        for(Integer grade : grades){
+            bucket += grade;
+        }
+        return bucket/ grades.size();
     }
 
 
-    // returns the student's id
-    public long getId();
-
-    // returns the student's name
-    public String getName();
-
-    // adds the given grade to the grades list
-    public void addGrade(int grade);
-
-    // returns the average of the students grades
-    public double getGradeAverage();
-
+    //GETTERS & SETTERS
+    public long getId(){
+        return this.id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String user) {
+        this.name = name;
+    }
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,14 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
-        import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student student;
+    Student shelby = new Student(324, "shelby");
+
+    @Before
+    public void setUp() {
+        shelby.addGrade(34);
+    }
+
+//    @Test
+//    public void testName() {
+//        assertEquals(name, Student.name);
+//    }
+//
+//    @Test
+//    public void testId() {
+//        assertEquals(id, Student.id);
+//    }
+
     @Test
-    public void testIdLongNum(){
-//        assertEquals(0, Student.user, 0);
-//        long test = Student.user;
-//        assertEquals(1.5, Student.user, 0);
-        assertTrue(long, Student.user);
+    public void testGrade(){
+        assertEquals(1, shelby.getGrades().size());
+
     }
 
 }
